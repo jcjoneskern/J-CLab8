b29ba3ed22e4470db4e883ea925f5ebf1e073059
J-C <jcjoneskern@gmail.com>
2016-11-07 13:02:56 -0500

    update

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 var groceryList = [];
 additem.onclick = addToList;
 
+
+//should rewrite this function using the insertRow()/insertCell() method
 function addToList() {
+
 	var total = 0;
 	var items = document.getElementById("newItem").value;
 	var prices = parseFloat(document.getElementById("newPrice").value);
@@ -9,21 +12,21 @@ function addToList() {
 	if (isNaN(prices)) {
 		document.getElementById("response").innerHTML = "Please enter a number for the price.";
 	} else {
+		groceryList.push(
+			{name: items,
+			price: prices});
 
-	groceryList.push(
-		{name: items,
-		price: prices});
+		var addedItem = document.createElement("tr");
+		addedItem.innerHTML = "<td>" + items + "</td><td>$" + prices.toFixed(2) +"</td>";
+		document.getElementById("tabledata").appendChild(addedItem);
 
-	var addedItem = document.createElement("tr");
-	addedItem.innerHTML = "<td>" + items + "</td><td>$" + prices.toFixed(2) +"</td>";
-	document.getElementById("tabledata").appendChild(addedItem);
+	//total amount
+	//make this a forEach instead
+		for(var i = 0; i < groceryList.length; i++) {
+			total += groceryList[i].price;
+		};
+		document.getElementById("totalvalue").innerHTML = "$" + total.toFixed(2);
 
-//total amount
-	for(var i = 0; i < groceryList.length; i++) {
-		total += groceryList[i].price;
-	};
-	document.getElementById("totalvalue").innerHTML = "$" + total.toFixed(2);
-
-	document.getElementById("response").innerHTML = "";
+		document.getElementById("response").innerHTML = "";
 	};
 };
